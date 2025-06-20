2e73cb7e9257de7d3501fbd93197a3360bf0d549
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-06-20 19:08:23 -0400

    Inicialization project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Home } from './pages/home/home';
+
+export const routes: Routes = [
+    {path: "login", component: Login},
+    {path: "register", component: Register},
+    {path: "", component: Home},
+];
