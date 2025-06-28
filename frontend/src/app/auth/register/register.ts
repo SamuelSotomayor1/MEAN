043040a77f18ab257166ba10a3b4e533ceccf5c7
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-06-28 19:21:54 -0400

    feat: the interface register are updated and the logic and connection with the backend are works

--- frontend/src/app/auth/register/register.ts
+++ frontend/src/app/auth/register/register.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  user = {
+    email: '',
+    password: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  register() {
+    this.http.post('http://localhost:3002/api/register', this.user).subscribe({
+      next: (res) => {
+        console.log('Usuario registrado correctamente', res);
+        // Redirige o muestra mensaje
+      },
+      error: (err) => {
+        console.error('Error al registrar usuario', err);
+      }
+    });
+  }
 }
