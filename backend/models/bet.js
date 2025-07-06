5460dec4fb18b3511a366f0bcbe34bb17694f11d
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-07-05 21:00:31 -0400

    feat: the model of bets are agregated

---
+++ backend/models/bet.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const betSchema = new mongoose.Schema({
+  match: {
+    type: String,
+    required: true
+  },
+  amountBet: {
+    type: Number,
+    required: true
+  },
+  amountWon: {
+    type: Number,
+    default: 0
+  },
+  status: {
+    type: String,
+    enum: ['ganado', 'perdido', 'cashout'],
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  notes: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Bet', betSchema);
---
+++ backend/routes/bet.route.js
@@ -0,0 +1,2 @@
+
+app.use("/bets", betUser);
